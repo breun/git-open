433c00b39461f7b5a88808392170142c974997ff
Nils Breunese <nils@breun.nl>
2021-08-25 23:59:51 +0200

    Refactor

--- common.sh
+++ common.sh
@@ -20,17 +20,17 @@ git_open () {
     local GITHUB_POSTFIX=$1
     local GITLAB_POSTFIX=$2
 
-    # Transforms Git URL to base URL
+    # Transform Git URL to browser base URL
     # Examples:
     #     git@gitlab.com:group/project.git => https://gitlab.com/group/project
     #     https://github.com:group/project.git => https://github.com/group/project
-    local BASE_URL=`git remote get-url "${REMOTE}" | sed 's|\.git$||' | sed -E 's;^(git@|https://);;' | sed 's|:|/|'`
+    local BASE_URL="https://$(git remote get-url "${REMOTE}" | sed 's|^git@||' | sed 's|^https://||' | sed 's|\.git$||' | sed 's|:|/|')"
 
-    if [[ $BASE_URL == *"github.com"* ]]; then
+    if [[ "${BASE_URL}" == "https://github.com/"* ]]; then
     	# Open GitHub URL
-    	"${OPEN_CMD}" "https://${BASE_URL}${GITHUB_POSTFIX}"
+    	"${OPEN_CMD}" "${BASE_URL}${GITHUB_POSTFIX}"
     else
     	# Assume we're dealing with GitLab
-    	"${OPEN_CMD}" "https://${BASE_URL}${GITLAB_POSTFIX}"
+    	"${OPEN_CMD}" "${BASE_URL}${GITLAB_POSTFIX}"
     fi
 }
